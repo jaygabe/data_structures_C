c098112b1e41b298baf6e4ec243aebcbfe0f5dba
James Peterson <omesadev@outlook.com>
2023-11-29 09:57:21 -0500

    Clarify 2807 solution

--- problems/medium/1_2807_gcd_linkedlist.c
+++ problems/medium/1_2807_gcd_linkedlist.c
@@ -32,29 +32,43 @@ struct ListNode *createNode(int val)
 {
 
     // Allocate space the size of the ListNode
-    struct ListNode *create = malloc(sizeof(struct ListNode));
-    create->val = val;
+    struct ListNode *newNode = malloc(sizeof(struct ListNode));
+    newNode->val = val;
     // Space still prereserved
-    create->next = NULL;
+    newNode->next = NULL;
     // Return pointer to 
-    return create;
+    return newNode;
 }
 
 struct ListNode* insertGreatestCommonDivisors(struct ListNode* head)
 {
+    // If there is no initial node or the first node does not have a next node
+    // Just return the head because there are not two nodes to place a GCD between
     if(head == NULL || head->next == NULL) return head;
 
+    // Create a reference to the head node we were given
     struct ListNode *curr = head;
+    // Create a reference to what will become the next node
     struct ListNode *next = NULL;
 
+    // While there is a node at our current node
+    // and there is a node at the next node position
     while(curr != NULL && curr->next != NULL) 
     {
+        // Store the node that comes after current node
+        // in our next variable
         next = curr->next;
 
+        // Find out what the GCD of our current and next node is
         int div = gcd(curr->val, curr->next->val);
+        // Use that GCD to create a brand new node
+        // We will place this between current and next nodes
         struct ListNode *temp = createNode(div);
+        // Point the temp->next pointer to our next node
         temp->next = curr->next;
+        // Point our curr->next pointer to our temp node
         curr->next = temp;
+        // Move current to our next node
         curr = next;
     } 
 
